44e9cb6c2020faf2d550940354b0b070067ad435
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2023-10-16 14:55:34 +0700

    update code to use recursion & data driven testing & green4ever again

--- src/main/java/com/VVT/birdservice/core/MathUtility.java
+++ src/main/java/com/VVT/birdservice/core/MathUtility.java
@@ -23,12 +23,52 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1;
         }
-
-        long result = 10;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        
+        return n*getFactorial(n-1);
     }
+    //RECURSION - RECURSION
+    //BÚP BÊ NGA - ĐỆ QUY LÀ GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+    //             NHỎ ĐẾN 1 MỨC THÌ DỪNG LẠI
+    //5! = 1.2.3.4... .5
+    //5! = 4! x 5 = 5 x 4!
+    //4! = 1.2.3.   4
+    //4! = 3! X 4 = 4 X 3! đuổi nhau rồi
+    //3!          = 3 x 2!
+    //2!          = 2 x 1! DỪNG NGAY
+    //N!          = N x (N-1)!
+    
+    //KĨ THUẬT KIỂM THỬ HỒI QUY - REGRESSION TEST
+    //TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST ĐỂ XÁC NHẬN NÓ CÒN CÓ ỔN KO
+    //TẠI SAO LẠI PHẢI TEST LẠI???
+    //CODE THEO 1 THỜI GIAN SẼ CÓ THAY ĐỔI, THAY ĐỔI VÌ
+    //1. ANH EM FIX BUG, SẼ SỬA CODE
+    //2. ANH EM TỐI ƯU, CHỈNH SUS7Ả CODE ĐỂ CODE ĐẸP HƠN, CHẠY TỐT HƠN
+    //   NHANH HƠN
+    //3. ANH EM THÊM HÀM MỚI, MOETHOD MỚI
+    
+    //KHI SỬA CODE, PHẢI TEST LẠI ĐỂ ĐẢM BẢO NÓ VẪN NGON, VẪN XANH
+    //NẾU TEST = TAY CỰC KÌ MẤT SỨC, COI CHỪNG SAI SÓT
+    //NẾU CÓ TEST SCRIPT, TA CHỈ VIỆC RUN, VÀ NHÌN MÀU ĐƯA RA
+    //NẾU CODE MÀU XANH, CODE CHỈNH SỬA ỔN, VÌ THOẢ EXPECTED == ACTUAL
+    //NẾU MÀU ĐỎ, CODE KO ỔN, SỬA TIẾP CHO ĐẾN KHI NÀO ỔN
+    //TA TEST LẠI MÀ CHỈ CẦN NHÌN MÀU -> NHANH, KO HAO SỨC, CHÍNH XÁC MIỄN ĐỦ TEST CASE, TEST SCRIPT
+    
+    //TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST GỌI LÀ TEST HỒI QUY REGRESSION
 
+    
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException(" Invalid n . n must be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
 }
